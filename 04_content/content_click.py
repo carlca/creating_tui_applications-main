869fe66164a4d3607df237f767c63ba59e047d2d
Carl Caulkett <carl.caulkett@gmail.com>
2025-08-30 16:20:23 +0100

    First commit.

---
+++ 04_content/content_click.py
@@ -0,0 +1,14 @@
+# content_click.py
+
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+
+class ContentClick(App):
+   def compose(self) -> ComposeResult:
+      yield Label("Play the [on green @click=app.bell]bells[/] today!")
+
+
+if __name__ == "__main__":
+   app = ContentClick()
+   app.run()
---
+++ 07_events/on_decorator.py
@@ -0,0 +1,30 @@
+# on_decorator.py
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Button
+
+
+class ButtonEvents(App):
+   def compose(self) -> ComposeResult:
+      yield Button("Toggle Dark Mode", classes="dark mode")
+      yield Button("Exit", id="exit")
+
+   @on(Button.Pressed, ".dark.mode")
+   def toggle_dark_mode(self) -> None:
+      """
+      Called when the "Toggle Dark Mode" button is pressed
+      """
+      self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
+
+   @on(Button.Pressed, "#exit")
+   def quit_app(self):
+      """
+      Called when the "Exit" button is pressed
+      """
+      self.exit()
+
+
+if __name__ == "__main__":
+   app = ButtonEvents()
+   app.run()
